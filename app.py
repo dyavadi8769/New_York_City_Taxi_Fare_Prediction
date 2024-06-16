e1c8d09ede32e0586b154799acadbefaa634b66c
dyavadi8769 <sdyavadi@tamu.edu>
2024-06-15 22:30:55 -0500

    Made changes in web app UI

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True) # running the app
+	app.run(host='127.0.0.5', port=8080,debug=True) # running the app
